ea71f523f49c497d7f16c15b442992552ae4251f
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2024-09-15 13:27:17 +0500

    model trainer completed

--- src/Wine_Quality_Prediction/entity/config_entity.py
+++ src/Wine_Quality_Prediction/entity/config_entity.py
@@ -24,17 +24,16 @@ class DataTransformationConfig:
     data_path: Path
 
 
-# @dataclass(frozen=True)
-# class ModelTrainerConfig:
-#     root_dir: Path
-#     train_data_path: Path
-#     test_data_path: Path
-#     model_name: str
-#     alpha: float
-#     l1_ratio: float
-#     target_column: str
-#
-#
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float
+    l1_ratio: float
+    target_column: str
+
 # @dataclass(frozen=True)
 # class ModelEvaluationConfig:
 #     root_dir: Path
