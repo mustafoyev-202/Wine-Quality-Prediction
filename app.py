7314b61e13e731a61a276cf4e1d29a3e068df45d
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2024-11-20 17:48:00 +0500

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import os
 import numpy as np
-import pandas as pd
 from Wine_Quality_Prediction.pipeline.prediction import PredictionPipeline
 
 app = Flask(__name__)  # initializing a flask app
